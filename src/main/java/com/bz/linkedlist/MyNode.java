7ee2ab535130bd58d17287d13efd308206ddf805
Subhamdas20 <subham21.sd@gmail.com>
2021-10-02 22:54:25 +0530

    created a simple linked list of 56 , 30 and 70

---
+++ src/main/java/com/bz/linkedlist/MyNode.java
@@ -0,0 +1,21 @@
+package com.bz.linkedlist;
+
+public class MyNode <T>{
+    private  MyNode next;
+    private T key;
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+
+    public MyNode(int key) {
+        this.key = null;
+        this.next = null;
+    }
+}
+
