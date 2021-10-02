07954ed606094f3b45a6f8a90c79c0e28230d18c
Subhamdas20 <subham21.sd@gmail.com>
2021-10-03 01:44:21 +0530

    created a simple linked list by appending 30 and 70 to 56

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -8,15 +8,30 @@ public class MyLinkedListTest {
     public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
         MyNode<Integer> myThirdNode = new MyNode(56);
         MyNode<Integer> mySecondNode = new MyNode(30);
-        MyNode<Integer> myFirstNode = new MyNode(70);
+        MyNode<Integer>  myFirstNode = new MyNode(70);
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add(myFirstNode);
         myLinkedList.add(mySecondNode);
         myLinkedList.add(myThirdNode);
-
         boolean result = myLinkedList.head.equals(myThirdNode) &&
                 myLinkedList.head.getNext().equals(mySecondNode)
                 && myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
+        myLinkedList.printNodes();
+    }
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAdded(){
+        MyNode<Integer>  myFirstNode = new MyNode(56);
+        MyNode<Integer> mySecondNode = new MyNode(30);
+        MyNode<Integer>  myThirdNode = new MyNode(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode)
+                && myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
         myLinkedList.printNodes();
     }
