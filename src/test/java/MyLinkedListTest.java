fb8e139ea6e7ffced2f662acf8b25fcac6111c1c
Subhamdas20 <subham21.sd@gmail.com>
2021-10-03 12:45:34 +0530

    deleted the first node and made second node as head

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
     @Test
-    public void given3NumbersOneIsInsertedBetweenTheTwoShouldPassTheResultTestCase(){
+    public void given3NumbersWhereFirstNodeIsDeletedShouldPassTheResultTestCase(){
         MyNode<Integer>  myFirstNode = new MyNode(56);
         MyNode<Integer> mySecondNode = new MyNode(30);
         MyNode<Integer>  myThirdNode = new MyNode(70);
@@ -13,9 +13,9 @@ public class MyLinkedListTest {
         myLinkedList.append(myFirstNode);
         myLinkedList.append(myThirdNode);
         myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.pop();
         myLinkedList.printNodes();
-        boolean result = myLinkedList.head.equals(myFirstNode) &&
-                myLinkedList.head.getNext().equals(mySecondNode)
+        boolean result = myLinkedList.head.equals(mySecondNode)
                 && myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
